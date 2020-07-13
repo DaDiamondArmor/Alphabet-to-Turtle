948d4d5192889e268f170d1e6a52934580999336
DaDiamondArmor <68228774+DaDiamondArmor@users.noreply.github.com>
2020-07-13 22:45:38 +0530

    Add files via upload

--- ALPH TO TURTLE/StartProg.py
+++ ALPH TO TURTLE/StartProg.py
@@ -1,10 +1,16 @@
 import turtle
 print("Welcome to ..uh... whatever this is...")
-print("This program will write any 3 letter word into turtle!")
+print("This program will write any 6 letter word into turtle! (less if you want; just put a slash in the next letters)")
 print("Please enter the letters in small form or else the program might not work.")
 l1= str(input("Enter the first letter:"))
 l2= str(input("Enter the sceond letter:"))
-l3= str(input("Enter the third letter"))
+l3= str(input("Enter the third letter:"))
+l4= str(input("Enter the fourth letter:"))
+l5= str(input("Enter the fifth letter:"))
+l6= str(input("Enter the sixth letter:"))
+
+from mdls import prepto
+
 
 if l1=="a":
 	from mdls import a
@@ -59,120 +65,286 @@ elif l1=="y":
 elif l1=="z":
 	from mdls import z
 else:
-	l4=234
+	l7=234
 
 
 if l2=="a":
-	from mdls import a
+	from mdls import a1
 elif l2=="b":
-	from mdls import b
+	from mdls import b1
 elif l2=="c":
-	from mdls import c
+	from mdls import c1
 elif l2=="d":
-	from mdls import d
+	from mdls import d1
 elif l2=="e":
-	from mdls import e
+	from mdls import e1
 elif l2=="f":
-	from mdls import f
+	from mdls import f1
 elif l2=="g":
-	from mdls import g
+	from mdls import g1
 elif l2=="h":
-	from mdls import h
+	from mdls import h1
 elif l2=="i":
-	from mdls import i
+	from mdls import i1
 elif l2=="j":
-	from mdls import j
+	from mdls import j1
 elif l2=="k":
-	from mdls import k
+	from mdls import k1
 elif l2=="l":
-	from mdls import l
+	from mdls import l1
 elif l2=="m":
-	from mdls import m
+	from mdls import m1
 elif l2=="n":
-	from mdls import n
+	from mdls import n1
 elif l2=="o":
-	from mdls import o
+	from mdls import o1
 elif l2=="p":
-	from mdls import p
+	from mdls import p1
 elif l2=="q":
-	from mdls import q
+	from mdls import q1
 elif l2=="r":
-	from mdls import r
+	from mdls import r1
 elif l2=="s":
-	from mdls import s
+	from mdls import s1
 elif l2=="t":
-	from mdls import t
+	from mdls import t1
 elif l2=="u":
-	from mdls import u
+	from mdls import u1
 elif l2=="v":
-	from mdls import v
+	from mdls import v1
 elif l2=="w":
-	from mdls import w
+	from mdls import w1
 elif l2=="x":
-	from mdls import x
+	from mdls import x1
 elif l2=="y":
-	from mdls import y
+	from mdls import y1
 elif l2=="z":
-	from mdls import z
+	from mdls import z1
 else:
-	l4=234
+	l7=234
 
 
 
 
 if l3=="a":
-	from mdls import a
+	from mdls import a2
 elif l3=="b":
-	from mdls import b
+	from mdls import b2
 elif l3=="c":
-	from mdls import c
+	from mdls import c2
 elif l3=="d":
-	from mdls import d
+	from mdls import d2
 elif l3=="e":
-	from mdls import e
+	from mdls import e2
 elif l3=="f":
-	from mdls import f
+	from mdls import f2
 elif l3=="g":
-	from mdls import g
+	from mdls import g2
 elif l3=="h":
-	from mdls import h
+	from mdls import h2
 elif l3=="i":
-	from mdls import i
+	from mdls import i2
 elif l3=="j":
-	from mdls import j
+	from mdls import j2
 elif l3=="k":
-	from mdls import k
+	from mdls import k2
 elif l3=="l":
-	from mdls import l
+	from mdls import l2
 elif l3=="m":
-	from mdls import m
+	from mdls import m2
 elif l3=="n":
-	from mdls import n
+	from mdls import n2
 elif l3=="o":
-	from mdls import o
+	from mdls import o2
 elif l3=="p":
-	from mdls import p
+	from mdls import p2
 elif l3=="q":
-	from mdls import q
+	from mdls import q2
 elif l3=="r":
-	from mdls import r
+	from mdls import r2
 elif l3=="s":
-	from mdls import s
+	from mdls import s2
 elif l3=="t":
-	from mdls import t
+	from mdls import t2
 elif l3=="u":
-	from mdls import u
+	from mdls import u2
 elif l3=="v":
-	from mdls import v
+	from mdls import v2
 elif l3=="w":
-	from mdls import w
+	from mdls import w2
 elif l3=="x":
-	from mdls import x
+	from mdls import x2
 elif l3=="y":
-	from mdls import y
+	from mdls import y2
 elif l3=="z":
-	from mdls import z
+	from mdls import z2
 else:
-	l4=234
+	l7=234
 
+if l4=="a":
+	from mdls import a3
+elif l4=="b":
+	from mdls import b3
+elif l4=="c":
+	from mdls import c3
+elif l4=="d":
+	from mdls import d3
+elif l4=="e":
+	from mdls import e3
+elif l4=="f":
+	from mdls import f3
+elif l4=="g":
+	from mdls import g3
+elif l4=="h":
+	from mdls import h3
+elif l4=="i":
+	from mdls import i3
+elif l4=="j":
+	from mdls import j3
+elif l4=="k":
+	from mdls import k3
+elif l4=="l":
+	from mdls import l3
+elif l4=="m":
+	from mdls import m3
+elif l4=="n":
+	from mdls import n3
+elif l4=="o":
+	from mdls import o3
+elif l4=="p":
+	from mdls import p3
+elif l4=="q":
+	from mdls import q3
+elif l4=="r":
+	from mdls import r3
+elif l4=="s":
+	from mdls import s3
+elif l4=="t":
+	from mdls import t3
+elif l4=="u":
+	from mdls import u3
+elif l4=="v":
+	from mdls import v3
+elif l4=="w":
+	from mdls import w3
+elif l4=="x":
+	from mdls import x3
+elif l4=="y":
+	from mdls import y3
+elif l4=="z":
+	from mdls import z3
+else:
+	l7=234	
+
+
+if l5=="a":
+	from mdls import a4
+elif l5=="b":
+	from mdls import b4
+elif l5=="c":
+	from mdls import c4
+elif l5=="d":
+	from mdls import d4
+elif l5=="e":
+	from mdls import e4
+elif l5=="f":
+	from mdls import f4
+elif l5=="g":
+	from mdls import g4
+elif l5=="h":
+	from mdls import h4
+elif l5=="i":
+	from mdls import i4
+elif l5=="j":
+	from mdls import j4
+elif l5=="k":
+	from mdls import k4
+elif l5=="l":
+	from mdls import l4
+elif l5=="m":
+	from mdls import m4
+elif l5=="n":
+	from mdls import n4
+elif l5=="o":
+	from mdls import o4
+elif l5=="p":
+	from mdls import p4
+elif l5=="q":
+	from mdls import q4
+elif l5=="r":
+	from mdls import r4
+elif l5=="s":
+	from mdls import s4
+elif l5=="t":
+	from mdls import t4
+elif l5=="u":
+	from mdls import u4
+elif l5=="v":
+	from mdls import v4
+elif l5=="w":
+	from mdls import w4
+elif l5=="x":
+	from mdls import x4
+elif l5=="y":
+	from mdls import y4
+elif l5=="z":
+	from mdls import z4
+else:
+	l7=234
+
+
+if l6=="a":
+	from mdls import a5
+elif l6=="b":
+	from mdls import b5
+elif l6=="c":
+	from mdls import c5
+elif l6=="d":
+	from mdls import d5
+elif l6=="e":
+	from mdls import e5
+elif l6=="f":
+	from mdls import f5
+elif l6=="g":
+	from mdls import g5
+elif l6=="h":
+	from mdls import h5
+elif l6=="i":
+	from mdls import i5
+elif l6=="j":
+	from mdls import j5
+elif l6=="k":
+	from mdls import k5
+elif l6=="l":
+	from mdls import l5
+elif l6=="m":
+	from mdls import m5
+elif l6=="n":
+	from mdls import n5
+elif l6=="o":
+	from mdls import o5
+elif l6=="p":
+	from mdls import p5
+elif l6=="q":
+	from mdls import q5
+elif l6=="r":
+	from mdls import r5
+elif l6=="s":
+	from mdls import s5
+elif l6=="t":
+	from mdls import t5
+elif l6=="u":
+	from mdls import u5
+elif l6=="v":
+	from mdls import v5
+elif l6=="w":
+	from mdls import w5
+elif l6=="x":
+	from mdls import x5
+elif l6=="y":
+	from mdls import y5
+elif l6=="z":
+	from mdls import z5
+else:
+	l7=234
 turtle.exitonclick()
